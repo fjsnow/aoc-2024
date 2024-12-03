ac225231321c8fd7f774d50531e5e9356e2ccd95
Freddy Snow <fjsn@protonmail.com>
2024-12-03 05:40:04 +0000

    i'm depressed

--- days/3/solve.py
+++ days/3/solve.py
@@ -1,18 +1,28 @@
-import sys
+import sys, re
 
 if len(sys.argv) > 1 and sys.argv[1] == "-r":
     with open("input", "r") as f:
         input = [l.rstrip("\n") for l in f.readlines()]
 else:
-    input = """""".split("\n")
+    input = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))""".split("\n")
 
-total = 0
+p1, p2 = 0, 0
+enabled = True
 
-for li, line in enumerate(input):
-    # for ci, char in enumerate(line):
-    #     pass
-    # for vi, val in enumerate(line.split("\n")):
-    #     pass
-    pass
+for line in input:
+    matches = re.findall(r'do\(\)|don\'t\(\)|mul\(\d{1,3},\d{1,3}\)', line)
+    for match in matches:
+        if match == "do()":
+            enabled = True
+        elif match == "don\'t()":
+            enabled = False
+        else:
+            nums = [int(x) for x in re.findall(r'\d+', match)]
 
-print("total:", total)
+            value = nums[0] * nums[1]
+            p1 += value
+            if enabled:
+                p2 += value
+
+print("p1", p1)
+print("p2", p2)
