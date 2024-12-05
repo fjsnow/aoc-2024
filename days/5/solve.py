eeeb3e726ece697b8aea96dcf0ab98ea8f4de1b2
Freddy Snow <fjsn@protonmail.com>
2024-12-05 05:49:36 +0000

    Replaced lists with sets where appropriate

--- days/5/solve.py
+++ days/5/solve.py
@@ -2,25 +2,25 @@ with open("input", "r") as f:
     input = "\n".join([l.rstrip("\n") for l in f.readlines()])
 
 rulesStr, updatesStr = [p.split("\n") for p in input.split("\n\n")]
-rules = [(rule.split("|")[0], rule.split("|")[1]) for rule in rulesStr]
+rules = set([(rule.split("|")[0], rule.split("|")[1]) for rule in rulesStr])
 updates = [update.split(",") for update in updatesStr]
 
 p1, p2 = 0, 0
 for i, update in enumerate(updates):
-    log = []
+    log = set()
     order = True
     for number in update:
         for l in log:
             if (number, l) in rules:
                 order = False
                 break
-        log.append(number)
+        log.add(number)
         if not order:
             break
     if order:
         p1 += int(update[len(update) // 2])
     else:
-        log, new = [], []
+        new = []
         for number in update:
             for li, l in enumerate(new):
                 if (number, l) in rules:
@@ -28,7 +28,6 @@ for i, update in enumerate(updates):
                     break
             else:
                 new.append(number)
-
         p2 += int(new[len(new) // 2])
 
 print("p1:", p1)
