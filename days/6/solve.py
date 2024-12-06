63e349616c3b0309eabed9fe9d93eb0af3f5d6cd
Freddy Snow <fjsn@protonmail.com>
2024-12-06 09:04:58 +0000

    optimise by not starting at the beginning, and rather one step before the obstacle would be reached cached from p1

--- days/6/solve.py
+++ days/6/solve.py
@@ -7,7 +7,7 @@ ds = { "U": (-1, 0, "R"), "D": (1, 0, "L"), "R": (0, 1, "D"), "L": (0, -1, "U") 
 gs = next(((ri, ci) for ri, r in enumerate(grid) for ci, v in enumerate(r) if v == "^"))
 
 # p1
-g, d, vi = gs, "U", set()
+g, d, vi, vid = gs, "U", set(), []
 vi.add(g)
 while True:
     n = (g[0] + ds[d][0], g[1] + ds[d][1])
@@ -17,29 +17,32 @@ while True:
         d = ds[d][2]
     else:
         g = n
-    vi.add(g)
+    if g not in vi:
+        vi.add(g)
+        vid.append((g, d))
 p1 = len(vi)
 
+pvid = vid
 # p2
-p = vi
-for ri, r in enumerate(grid):
-    for ci, v in enumerate(r):
-        if v == "#" or (ri, ci) not in p:
-            continue
-        g, d, vi = gs, "U", set()
-        vi.add((g, d))
-        while True:
-            n = (g[0] + ds[d][0], g[1] + ds[d][1])
-            if n[0] < 0 or n[0] > len(grid) - 1 or n[1] < 0 or n[1] > len(grid[n[0]]) - 1:
-                break
-            if grid[n[0]][n[1]] == "#" or (n[0] == ri and n[1] == ci):
-                d = ds[d][2]
-            else:
-                g = n
-            if (g, d) in vi:
-                p2 += 1
-                break
-            vi.add((g, d))
+for i, ((ri, ci), d) in enumerate(pvid):
+    if i == 0:
+        g, d, vid = gs, "U", set()
+    else:
+        l = pvid[i-1]
+        g, d, vid = l[0], l[1], set()
+    vi.add((g, d))
+    while True:
+        n = (g[0] + ds[d][0], g[1] + ds[d][1])
+        if n[0] < 0 or n[0] > len(grid) - 1 or n[1] < 0 or n[1] > len(grid[n[0]]) - 1:
+            break
+        if grid[n[0]][n[1]] == "#" or (n[0] == ri and n[1] == ci):
+            d = ds[d][2]
+        else:
+            g = n
+        if (g, d) in vid:
+            p2 += 1
+            break
+        vid.add((g, d))
 
 print("p1", p1)
 print("p2", p2)
