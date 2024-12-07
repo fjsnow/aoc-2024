d8b892381af7fa0cf30f94ad89efec6095cfeda6
Freddy Snow <fjsn@protonmail.com>
2024-12-07 04:58:16 +0000

    sieben

--- days/6/solve.py
+++ days/6/solve.py
@@ -1,3 +1,4 @@
+import time
 with open("input", "r") as f:
     grid = [l.rstrip("\n") for l in f.readlines()]
 
@@ -33,7 +34,7 @@ p1_visited = visited # used for optimisation in p2
 
 # p2
 for i, (ri, ci) in enumerate(p1_visited.keys()):
-    if i == 0:
+    if i == 0 or True:
         guard, direction, visited = guard_start, "U", set()
     else:
         last_guard = list(p1_visited.items())[i-1] # python dictionaries are ordered based on insertion yippee
@@ -42,19 +43,23 @@ for i, (ri, ci) in enumerate(p1_visited.keys()):
     visited.add((guard, direction))
 
     while True:
+        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        b = time.perf_counter()
         direction_offset, direction_right = directions[direction]
         next_guard = (guard[0] + direction_offset[0], guard[1] + direction_offset[1])
         if next_guard[0] < 0 or next_guard[0] > len(grid) - 1 or next_guard[1] < 0 or next_guard[1] > len(grid[next_guard[0]]) - 1:
             break
+
         if grid[next_guard[0]][next_guard[1]] == "#" or (next_guard[0] == ri and next_guard[1] == ci):
             direction = direction_right
         else:
             guard = next_guard
+
         if (guard, direction) in visited:
             p2 += 1
             break
 
         visited.add((guard, direction))
-
+        print("in took", time.perf_counter() - b)
 print("p1", p1)
 print("p2", p2)
