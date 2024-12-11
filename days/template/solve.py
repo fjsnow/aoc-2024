d221455ba358809ac6f6c3c036c3a2d32295de14
Freddy Snow <fjsn@protonmail.com>
2024-12-11 06:06:21 +0000

    rename input to inp in template, as sometimes i use input to step through itreations

--- days/template/solve.py
+++ days/template/solve.py
@@ -2,13 +2,13 @@ import sys
 
 if len(sys.argv) > 1 and sys.argv[1] == "-r":
     with open("input", "r") as f:
-        input = [l.rstrip("\n") for l in f.readlines()]
+        inp = [l.rstrip("\n") for l in f.readlines()]
 else:
-    input = """""".split("\n")
+    inp = """""".split("\n")
 
 total = 0
 
-for li, line in enumerate(input):
+for li, line in enumerate(inp):
     # for ci, char in enumerate(line):
     #     pass
     # for vi, val in enumerate(line.split(" ")):
