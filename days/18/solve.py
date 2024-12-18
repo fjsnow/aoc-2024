bdcf4a09171ac057d1e4a445cbcbbf0113cd757b
Freddy Snow <fjsn@protonmail.com>
2024-12-18 15:23:46 +0000

    binary search

--- days/18/solve.py
+++ days/18/solve.py
@@ -8,26 +8,41 @@ for line in inp:
     x, y = [int(x) for x in line.split(",")]
     corrupted.append((x, y))
 
-for i in range(1024, len(corrupted)):
-    q = [(0, (0, 0))]
+
+def djikstra(start, corrupted):
+    q = [(0, start)]
     heapq.heapify(q)
     b = set()
-    while len(q) > 0:
+    while q:
         c, (cx, cy) = heapq.heappop(q)
         if (cx, cy) == (70, 70):
-            if i == 1024:
-                print("p1:", c)
-            break
+            return c, (cx, cy)
         for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
             nx, ny = cx + dx, cy + dy
             if (nx, ny) in b:
                 continue
             if nx < 0 or ny < 0 or nx > 70 or ny > 70:
                 continue
-            if (nx, ny) in corrupted[:i]:
+            if (nx, ny) in corrupted:
                 continue
-            heapq.heappush(q, (c+1, (nx, ny)))
             b.add((nx, ny))
+
+            heapq.heappush(q, (c+1, (nx, ny)))
+    return None
+
+
+first = djikstra((0, 0), corrupted[:1024])
+assert first is not None
+
+print("p1:", first[0])
+
+low, high = 1024, len(corrupted)
+while low < high:
+    mid = (low + high) // 2
+    result = djikstra((0, 0), corrupted[:mid])
+    if result is None:
+        high = mid - 1
     else:
-        print("p2:", f"{corrupted[i-1][0]},{corrupted[i-1][1]}")
-        break
+        low = mid + 1
+
+print("p2:", corrupted[low])
