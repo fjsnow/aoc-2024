a3d96be9fac3b9602a240ee9eb1c7167745888b1
Freddy Snow <fjsn@protonmail.com>
2024-12-19 11:22:29 +0000

    Day 19

---
+++ days/19/solve.py
@@ -0,0 +1,27 @@
+from functools import cache
+
+with open("input", "r") as f:
+    inp = [l.rstrip("\n") for l in f.read().split("\n\n")]
+
+patterns = inp[0].split(", ")
+
+
+@cache
+def solve(towel, p2):
+    if len(towel) == 0:
+        return 1
+    c = 0
+    for p in patterns:
+        if towel[:len(p)] == p:
+            n = solve(towel[len(p):], p2)
+            if p2:
+                c += n if p2 else (1 if n == 1 else 0)
+            else:
+                if n == 1:
+                    c += 1
+                    break
+    return c
+
+
+print("p1:", sum(solve(t, False) for t in inp[1].split("\n")))
+print("p2:", sum(solve(t, True) for t in inp[1].split("\n")))
